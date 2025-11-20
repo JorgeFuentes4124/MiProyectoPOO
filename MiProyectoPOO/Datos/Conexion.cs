0bd11b0ab71f9a8e05f71cae21798374ac19e98a
Agustin Castro <agus.castro.sosa@gmail.com>
2025-11-20 17:02:11 -0300

    Agregar archivos de proyecto.

---
+++ MiProyectoPOO/Datos/Conexion.cs
@@ -0,0 +1,48 @@
+﻿
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiProyectoPOO.Datos
+{
+    class Conexion
+    {
+        MySqlConnection conexion = new MySqlConnection();
+        static string servidor = "localhost";
+        static string bd = "sistema_entradas";
+        static string usuario = "root";
+        static string password = "";
+        static string puerto = "3306";
+        string cadenaConexion =
+                        "server=" + servidor +
+                        ";database=" + bd +
+                        ";user=" + usuario +
+                        ";password=" + password +
+                        ";port=" + puerto + ";";
+
+
+        public MySqlConnection establecerConexion()
+        {
+            try {
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+                MessageBox.Show("Se Conecto a la BD Correctamente");
+            }
+
+            catch (MySqlException e){
+                MessageBox.Show("No se Conecto Correctamente a la BD" + e.ToString());
+            }
+                 
+            return conexion;
+        }
+
+        
+
+    }
+}
+
---
+++ MiProyectoPOO/Formularios/FormConexion.cs
@@ -0,0 +1,34 @@
+﻿using MiProyectoPOO.Datos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiProyectoPOO.Formularios
+{
+    public partial class FormConexion : Form
+    {
+        private Conexion nConexion;
+        public FormConexion()
+        {
+            InitializeComponent();
+            nConexion = new Conexion();
+        }
+
+        private void FormConexion_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Datos.Conexion conexion = new Datos.Conexion();
+            conexion.establecerConexion();
+        }
+    }
+}
